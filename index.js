7c37f9d5403b80e23531f5ec140a27ffa1b0733e
rtestard <rtestard@opensense.fr>
2019-11-05 11:06:40 +0100

    Formating

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-/* const myCustomObject = (type, filter, filterType) => {
-    const object = {queryNumber: { type, filter, filterType}} 
-    console.log(object)
-}
+/* EXPECTED RESULT FORMAT:
+{ operator: 'OR',
+  condition1: { type: 'equals', filter: 'BE', filterType: 'text' },
+  condition2: { type: 'equals', filter: 'LUX', filterType: 'text' },
+  condition3: { type: 'equals', filter: 'FR', filterType: 'text' } } 
+*/
 
-myCustomObject("equals", "BE", "text")
- */
+const initialValues = ["BE", "LUX", "FR"];
+let operator = { operator: "OR" };
 
-const selectedTargets = ['BE', 'LUX', 'FR']
+const queries = initialValues.map((target, index) => {
+  let queryNumber = "condition" + (index + 1);
+  return {
+    [queryNumber]: { type: "equals", filter: target, filterType: "text" }
+  };
+});
 
-const queries = selectedTargets.map((target, index) => {
-    let queryNumber = "condition" + (index + 1)
-    return {[queryNumber]: {type: 'equals', filter: target, filterType: "text"}}
-})
-let operator = {operator: 'OR'}
+console.log(queries)
 
-
-/* queries.forEach(e => {
-   return Object.entries(e).map(([k, v]) => {
-     operator[k] = v
+console.log(queries.forEach(e => {
+   return Object.entries(e).map((e) => {
+     console.log(e)
    })
-})
+}))
 
-console.log(operator)
- */
 
- const queriesR = selectedTargets.reduce((acc, target, index) => {
+/*  const queriesR = selectedTargets.reduce((acc, target, index) => {
     let queryNumber = "condition" + (index + 1)
     acc[queryNumber] = {type: 'equals', filter: target, filterType: "text"}
     return acc 
  }, operator)
 
- console.log(queriesR)
+ console.log(queriesR) */
